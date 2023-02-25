4e869c60a7b53077b04f7294cb7a99f8c33c240c
frmatias <vandembergneto@gmail.com>
2023-02-25 13:42:59 -0300

    adding category controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,12 @@
 import { Router } from "express";
-
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(
-        categoriesRepository
-    );
-
-    createCategoryService.execute({ description, name });
-    return response.status(201).send();
+    return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {

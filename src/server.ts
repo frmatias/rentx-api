23eaa050eed5e9698f2333f39a79686ca5613ed9
frmatias <vandembergneto@gmail.com>
2023-02-28 23:36:38 -0300

    adding swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
+// eslint-disable-next-line import-helpers/order-imports
 import express from "express";
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import swaggerUi from "swagger-ui-express";
+
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 

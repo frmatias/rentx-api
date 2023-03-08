fac989dd0e5ce30708e82faa4caf402449a9a15f
frmatias <vandembergneto@gmail.com>
2023-03-07 23:31:29 -0300

    adding user use csa controlller and routes

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,31 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({
+        name,
+        username,
+        email,
+        driver_license,
+        password,
+    }: ICreateUserDTO) {
+        await this.usersRepository.create({
+            name,
+            username,
+            email,
+            driver_license,
+            password,
+        });
+    }
+}
+
+export { CreateUserUseCase };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { container } from "tsyringe";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
@@ -14,4 +16,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 );

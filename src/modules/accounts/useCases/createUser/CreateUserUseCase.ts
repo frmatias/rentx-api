be53347902c2848010fc269e9c9c8ede1a1361f7
frmatias <vandembergneto@gmail.com>
2023-03-08 22:33:16 -0300

    fixing create user case

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -16,7 +16,7 @@ class CreateUserUseCase {
     async execute({ name, email, driver_license, password }: ICreateUserDTO) {
         const userAlreadyExists = await this.usersRepository.findByEmail(email);
         if (userAlreadyExists) {
-            throw new Error(`User ${name} already exists);
+            throw new Error(`User ${name} already exists`);
         }
         const passwordHash = await hash(password, 8);
         await this.usersRepository.create({

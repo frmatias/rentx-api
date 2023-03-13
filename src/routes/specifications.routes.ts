437088da0273f08c98e7375f9502de057f3244f0
frmatias <vandembergneto@gmail.com>
2023-03-12 21:03:15 -0300

    adding auth in specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
